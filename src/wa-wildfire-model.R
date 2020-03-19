a5a384d776650e4270170d1c8b0b08cf2df8e4a8
Bradley Reeves <bradleyaaronreeves@gmail.com>
2020-03-18 20:52:57 -0700

    added elimination methods/cleanup

--- src/wa-wildfire-model.R
+++ src/wa-wildfire-model.R
@@ -2,6 +2,7 @@
 # setwd("C:/Users/Coder/OneDrive/Documents/CS 567 Computational Stats/CS567_Project_2")
 
 library(plyr)
+library(reshape2)
 library(standardize)
 library(stringr)
 library(ggplot2)
@@ -36,8 +37,8 @@ for (cause in causes)
 
 for (row in 1:nrow(data))
 {
-  # There's probabely a better than this:
-  if (data[row, "Evacuations"] == 0 )
+  # some of the evacuated NAs can be set to 0
+  if (data[row, "Evacuations"] == 0)
   {
     data[row, "X..of.Evacuated"] = as.numeric(0)
   }
@@ -46,62 +47,112 @@ for (row in 1:nrow(data))
   data[row, paste(data[row, "Cause"])]  <- as.numeric(1)
 }
 
-# remove unnecessary column
-# NOTE: X..of.Sheltering.in.place is removed because it only has two rows of data, but otherwise NA
-data = subset(data, select=-c(County, Cause, Fire.Name, Date.Start, Date.contained, X..of.Sheltering.in.place))
+# remove unnecessary columns
+# note: X..of.Sheltering.in.place is removed because it only has two rows of data
+data = subset(data, select = -c(County, Cause, Fire.Name, Date.Start, Date.contained, X..of.Sheltering.in.place))
 
-# Remove ',' from values in acres and total costs columns then convert to numeric
+# remove ',' from values in acres and total costs columns then convert to numeric
 data$Size..acres. <- as.numeric(gsub(",","",data$Size..acres.))
 data$Estimated.Total.Cost <- as.numeric(gsub(",","",data$Estimated.Total.Cost))
 
 data<-na.omit(data) 
 
-# Make sure all columns are numeric
+# make sure all columns are numeric
 data <- as.data.frame(sapply(data, as.numeric))
 
-# NOTE: the na.rm=TRUE, above, takes care of the evacuations that occured be is NA
-# However, when sapply is used here it increments the values in Evacuations (assuming we care if normalized)
-
-#View(data)
-
-sdata = subset(data, select=-c(Estimated.Total.Cost))
-# Standardize all columns
+# remove outcome variable, standardize columns, then append outcome variable
+sdata = subset(data, select = -c(Estimated.Total.Cost))
 sdata = as.data.frame(scale(sdata))
-sdata = cbind(sdata,  subset(data, select=c(Estimated.Total.Cost)))
+sdata = cbind(sdata,  subset(data, select = c(Estimated.Total.Cost)))
 
-# View standardized dataframe
-#View(sdata)
+# intial model with all columns
+fit_all <- lm(Estimated.Total.Cost ~ ., data = sdata)
 
-# Backwards Elimination Example Used From:
-# https://www.youtube.com/watch?v=0aTtMJO-pE4
-FitAll <- lm(Estimated.Total.Cost ~ . , data=sdata)
+## backward elimination ##
+#step(fit_all, direction = "backward")
 
-#summary(FitAll)
-
-# Do backwards elimination of columns (Eliminates non-significant columns)
-step(FitAll, direction = "backward")
-
-selData = sdata[c("Estimated.Total.Cost","Resources.assigned..personnel.", "Structures.lost", "Lewis", "Deaths", "Injuries..Responders.",
+sel_data = sdata[c("Estimated.Total.Cost","Resources.assigned..personnel.", "Structures.lost", "Lewis", "Deaths", "Injuries..Responders.",
                   "Ferry", "Chelan", "Whatcom", "X..of.Evacuated", "Okanogan", "Lincoln", "Spokane", "Douglas", "Benton", "Year", "King",
                   "Size..acres.", "Structures.threatened", "Timber", "Yakima", "U")]
 
-index <- sample(1:nrow(selData), 0.8*nrow(selData))
-train_df <- selData[index, ]
-test_df  <- selData[-index, ]
+index <- sample(1:nrow(sel_data), 0.8*nrow(sel_data))
+train_df <- sel_data[index, ]
+test_df  <- sel_data[-index, ]
 
-model <- lm(Estimated.Total.Cost ~ ., data=train_df)
+model <- lm(Estimated.Total.Cost ~ ., data = train_df)
 
-pred.int <- predict(model, test_df, interval = "confidence")
-print(pred.int)
-
-pred.df <- cbind(test_df, pred.int)
-pred.df$rid <- as.numeric(row.names(pred.df))
+pred_int <- predict(model, test_df, interval = "confidence")
+pred_df <- cbind(test_df, pred_int)
+pred_df$rid <- as.numeric(row.names(pred_df))
 
 pd <- position_dodge(0.1)
 
-ggplot(pred.df, aes(x=rid, y=Estimated.Total.Cost, group=1)) + 
-  geom_errorbar(aes(ymin=lwr, ymax=upr), colour="black", width=.1, position=pd) +
-  geom_line(position=pd, color="blue", size=1) +
-  geom_point(position=pd, size=3) +
-  geom_line(aes(x=rid, y=fit, group=2), color="red", size=1)
+plot_backward <- ggplot(pred_df, aes(x = rid, y = Estimated.Total.Cost)) + 
+  geom_errorbar(aes(ymin = lwr, ymax = upr), colour = "black", width = .1, position = pd) +
+  geom_line(position = pd, color = "blue", size = 1.25) +
+  geom_point(position = pd, size = 2, color = "blue") +
+  geom_line(aes(x = rid, y = fit), color = "red", size = 1.25) +
+  geom_point(aes(x = rid, y = fit), size = 2, color = "red") + 
+  labs(title = "Model Accuracy with Backward Elimination", y = "Estimated Cost ($)", x = "Data Row")
+ggsave("out/back_elim.pdf", units = "in", width = 10, height = 5, dpi = 300)
+
+## forward elimination ##
+#step(fit_all, direction = "forward")
+
+sel_data = sdata[c("Estimated.Total.Cost","Year", "Size..acres.", "Grass", "Brush", "Timber",
+                        "Structures.lost", "Structures.damaged", "Structures.threatened", "Deaths", "Injuries..Responders.", 
+                        "Injuries..civilians.", "Evacuations", "X..of.Evacuated", "Resources.assigned..personnel.", 
+                        "duration", "Spokane", "Whitman", "Okanogan", "Whatcom", "Chelan", "Walla Walla", "Yakima", "Douglas", 
+                        "Klickitat", "Lincoln", "Ferry", "Asotin", "Snohomish", "Grant", "Stevens", "Lewis", "Benton", 
+                        "Franklin", "Pend Oreille", "King", "Kittitas", "H", "U", "L")]
+
+index <- sample(1:nrow(sel_data), 0.8*nrow(sel_data))
+train_df <- sel_data[index, ]
+test_df  <- sel_data[-index, ]
+
+model <- lm(Estimated.Total.Cost ~ ., data = train_df)
+
+pred_int <- predict(model, test_df, interval = "confidence")
+pred_df <- cbind(test_df, pred_int)
+pred_df$rid <- as.numeric(row.names(pred_df))
+
+pd <- position_dodge(0.1)
+
+plot_forward <- ggplot(pred_df, aes(x = rid, y = Estimated.Total.Cost)) + 
+  geom_errorbar(aes(ymin = lwr, ymax = upr), colour = "black", width = .1, position = pd) +
+  geom_line(position = pd, color = "blue", size = 1.25) +
+  geom_point(position = pd, size = 2, color = "blue") +
+  geom_line(aes(x = rid, y = fit), color = "red", size = 1.25) +
+  geom_point(aes(x = rid, y = fit), size = 2, color = "red") + 
+  labs(title = "Model Accuracy with Forward Elimination", y = "Estimated Cost ($)", x = "Data Row")
+ggsave("out/for_elim.pdf", units = "in", width = 10, height = 5, dpi = 300)
+
+## Both ##
+#step(fit_all, direction = "both")
+
+sel_data = sdata[c("Estimated.Total.Cost","Year", "Timber", "Structures.lost", "Deaths", "Injuries..Responders.",
+                        "X..of.Evacuated", "Resources.assigned..personnel.", "Spokane", "Okanogan", 
+                        "Whatcom", "Chelan", "Douglas", "Lincoln", "Ferry", "Lewis", "Benton",
+                        "Kittitas", "U")]
+
+index <- sample(1:nrow(sel_data), 0.8*nrow(sel_data))
+train_df <- sel_data[index, ]
+test_df  <- sel_data[-index, ]
+
+model <- lm(Estimated.Total.Cost ~ ., data = train_df)
+
+pred_int <- predict(model, test_df, interval = "confidence")
+pred_df <- cbind(test_df, pred_int)
+pred_df$rid <- as.numeric(row.names(pred_df))
+
+pd <- position_dodge(0.1)
+
+plot_both <- ggplot(pred_df, aes(x = rid, y = Estimated.Total.Cost)) + 
+  geom_errorbar(aes(ymin = lwr, ymax = upr), colour = "black", width = .1, position = pd) +
+  geom_line(position = pd, color = "blue", size = 1.25) +
+  geom_point(position = pd, size = 2, color = "blue") +
+  geom_line(aes(x = rid, y = fit), color = "red", size = 1.25) +
+  geom_point(aes(x = rid, y = fit), size = 2, color = "red") + 
+  labs(title = "Model Accuracy with Dual Elimination", y = "Estimated Cost ($)", x = "Data Row")
+ggsave("out/dual_elim.pdf", units = "in", width = 10, height = 5, dpi = 300)
 
